fceeef338ddb2baa0a6943b9c3daf13223239368
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-03-11 21:14:06 -0300

    CSS e outras operações

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -6,12 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './form-calc.component.css'
 })
 export class FormCalcComponent {
+  operation : string = "";
   num1 : number = 0;
   num2 : number = 0;
-  sum :  number = 0; 
+  result :  number = 0;
+  sinal : string = "";
+  
+  select(){
+    if(this.operation == "soma"){
+      this.sinal = "+"
+    }
+    if(this.operation == "subtração"){
+      this.sinal = "-"
+    }
+    if(this.operation == "multiplicação"){
+      this.sinal = "*"
+    }
+    if(this.operation == "divisão"){
+      this.sinal = "/"
+    }
+    if(this.operation == "potência"){
+      this.sinal = "**"
+    }
+  }
   
   onClickSum(){
-    this.sum = this.num1 + this.num2
+    if(this.operation == "soma"){
+      this.result = this.num1+this.num2;
+    }
+    if(this.operation == "subtração"){
+      this.result = this.num1-this.num2;
+    }
+    if(this.operation == "multiplicação"){
+      this.result = this.num1*this.num2;
+    }
+    if(this.operation == "divisão"){
+      this.result = this.num1/this.num2;
+    }
+    if(this.operation == "potência"){
+      this.result = this.num1**this.num2;
+    }
   }
 }
 
